2255be6dbda52293833c04068daf0e5c1f2d92b8
servao <80340966+servao@users.noreply.github.com>
2024-09-08 08:12:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
+#echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
+echo 'src-git  passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
+#src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main
+
